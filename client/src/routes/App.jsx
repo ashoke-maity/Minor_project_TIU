6f05ca721f973ad84567e9d5e2ce405c20824729
Abir-dev <abirlalbanerjee44@gmail.com>
2025-04-27 23:08:40 +0530

    Implement the AuthForm and made the Sign in and Sign up page

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../pages/Home';
-// import Login from '../components/others/Login';
-// import Register from '../components/others/Register';
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
 // import AdminLogin from '../pages/Admin';
 
 // const adminRoute = import.meta.env.ALUMNI_CONNECT_ADMIN_ROUTE;
@@ -12,8 +12,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/register" element={<Register/>} /> */}
-      {/* <Route path="/login" element={<Login/>} /> */}
+      <Route path="/SignIn" element={<SignIn/>} />
+      <Route path="/SignUp" element={<SignUp/>} />
       {/* <Route path = {`/${adminRoute}`} element={<AdminLogin/>} /> */}
     </Routes>
   </BrowserRouter>
