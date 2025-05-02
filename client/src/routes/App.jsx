3b89fd6a06f731d7583ddf45041f75e92b879764
ashoke-maity <ashokemaity092@gmail.com>
2025-05-02 14:04:43 +0530

    fetched the user data and shown it to the user home page
    made the user setting page

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../components/others/Login';
 import Register from '../components/others/Register';
+import UserSettings from '../pages/userSettings';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />}/>
+        <Route path="/dashboard" element={<UserSettings/>} />
       </Routes>
     </Router>
   );
