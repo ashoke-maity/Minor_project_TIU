2b0340e595cf0e7a00327b625b9776450206e8b9
ashoke-maity <ashokemaity092@gmail.com>
2025-05-30 23:26:03 +0530

    implemented like save comment api

--- server/routes/UserPostRoutes.js
+++ server/routes/UserPostRoutes.js
@@ -1,19 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verifyUserToken = require('../middlewares/userAuthMiddleware');
-const {createPost, getUserPosts, getAllPosts, deletePost} = require('../controllers/UserPostController');
-const upload = require('../middlewares/uploadMiddleware');
+const verifyUserToken = require("../middlewares/userAuthMiddleware");
+const {
+  createPost,
+  getUserPosts,
+  getAllPosts,
+  deletePost,
+  likePost,
+  unlikePost,
+  commentOnPost,
+  deleteComment,
+  savePost,
+  unsavePost,
+} = require("../controllers/UserPostController");
+const upload = require("../middlewares/uploadMiddleware");
 
 // create post
-router.post("/create/post", verifyUserToken, upload.single('media'), createPost)
+router.post(
+  "/create/post",
+  verifyUserToken,
+  upload.single("media"),
+  createPost
+);
 
 // see the other users posts
-router.get("/view/others", verifyUserToken, getUserPosts)
+router.get("/view/others", verifyUserToken, getUserPosts);
 
 // see own post
-router.get("/view/post", verifyUserToken, getAllPosts)
+router.get("/view/post", verifyUserToken, getAllPosts);
 
 // delete post
 router.delete("/delete/post/:id", verifyUserToken, deletePost);
+
+// like a post
+router.post("/like/post/:id", verifyUserToken, likePost);
+
+// unlike a post
+router.post("/unlike/post/:id", verifyUserToken, unlikePost);
+
+// comment on a post
+router.post(
+  "/comment/post/:id",
+  verifyUserToken,
+  commentOnPost
+);
+
+// delete a comment
+router.delete(
+  "/delete/comment/:postId/:commentId",
+  verifyUserToken,
+  deleteComment
+);
+
+// save a post
+router.post("/save/post/:id", verifyUserToken, savePost);
+
+// unsave a post
+router.post("/unsave/post/:id", verifyUserToken, unsavePost);
 
 module.exports = router;
